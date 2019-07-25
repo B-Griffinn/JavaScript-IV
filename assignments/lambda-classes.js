b39794b8fecec1c2a18d48828e402e3e3144b3c5
Ben <bigriffin94@outlook.com>
2019-07-25 13:15:32 -0700

    Refactor finished.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,36 @@
 // CODE here for your Lambda Classes
-ChannelMergerNode
+class GameObject{
+    constructor(gameObjectAttr) {
+        this.createdAt = gameObjectAttr.createdAt;
+        this.name = gameObjectAttr.name;
+        this.dimensions = gameObjectAttr.dimensions;
+    };
+//methods
+    destroy() {
+        return ` ${this.name} was removed from the game.`;
+    };
+};
+
+class CharacterStats extends GameObject {
+    constructor(charAttrs) {
+        super(charAttrs);
+        this.healthPoints = charAttrs.healthPoints;
+    };
+    //methods
+    takeDamage() {
+        return ` ${this.name} took damage.`;
+    };
+};
+
+class Humanoid extends CharacterStats {
+    constructor(humanoidAttrs){
+        super(humanoidAttrs);
+        this.team = humanoidAttrs.team;
+        this.weapons = humanoidAttrs.weapons;
+        this.language = humanoidAttrs.language;
+    };
+    //methods
+    greet() {
+        return ` ${this.name} offers a greeting in ${this.language}`;
+    };
+};
